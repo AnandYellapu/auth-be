2a3f8b1e53f517e1ec2b80cac8728674cda029ce
Yellapu AnandSai <104178105+AnandYellapu@users.noreply.github.com>
2023-07-02 09:32:44 +0530

    Update userController.js

--- controller/userController.js
+++ controller/userController.js
@@ -47,44 +47,87 @@ const getProfile = (req, res) => {
 
 
 
+// const forgotPassword = async (req, res) => {
+//   const token = await crypto.randomBytes(20).toString("hex");
+//   const user = await User.findOne({ email: req.body.email });
+
+//   if (!user) {
+//     return res.status(400).json({ error: "No user with such email!" });
+//   }
+
+//   user.resetPasswordToken = token;
+//   user.resetPasswordExpires = Date.now() + 3600000;
+
+//   try {
+//     await user.save();
+
+//     let transporter = nodemailer.createTransport({
+//       host: "smtp.ethereal.email",
+//       service: "gmail",
+//       port: 465,
+//       secure: true,
+//       auth: {
+//         user: "anandsaiii1200@gmail.com",
+//         pass: "azjtjuhdytbpdcfn",
+//       },
+//     });
+
+//     let info = await transporter.sendMail({
+//       from: `anandsaiii1200@gmail.com`,
+//       to: user.email,
+//       subject: "AUTH - Reset Password",
+//       text: `You are receiving this because you have requested the reset of the password of your account.\n\nToken: ${token}\n\nIf you didn't request this, please ignore this email and your password will remain unchanged.`,
+//       html: `<p>You are receiving this because you have requested the reset of the password of your account.</p><p><strong>Token: ${token}</strong></p><p>If you didn't request this, please ignore this email and your password will remain unchanged.</p>`,
+//     });
+
+//     return res.json({
+//       message: `An email has been sent to ${user.email} with further instructions`,
+//     });
+//   } catch (error) {
+//     return res.status(400).json({ error: error.message });
+//   }
+// };
+
 const forgotPassword = async (req, res) => {
-  const token = await crypto.randomBytes(20).toString("hex");
-  const user = await User.findOne({ email: req.body.email });
-
-  if (!user) {
-    return res.status(400).json({ error: "No user with such email!" });
-  }
-
-  user.resetPasswordToken = token;
-  user.resetPasswordExpires = Date.now() + 3600000;
-
   try {
+    const token = crypto.randomBytes(20).toString('hex');
+    const user = await User.findOne({ email: req.body.email });
+
+    if (!user) {
+      return res.status(400).json({ error: 'No user with such email!' });
+    }
+
+    user.resetPasswordToken = token;
+    user.resetPasswordExpires = Date.now() + 3600000;
+
     await user.save();
 
-    let transporter = nodemailer.createTransport({
-      host: "smtp.ethereal.email",
-      service: "gmail",
-      port: 465,
-      secure: true,
+    const transporter = nodemailer.createTransport({
+      // Configure your email service provider settings
+      service: 'gmail',
       auth: {
-        user: "anandsaiii1200@gmail.com",
-        pass: "azjtjuhdytbpdcfn",
+        user: 'anandsaiii1200@gmail.com',
+        pass: 'azjtjuhdytbpdcfn',
       },
     });
 
-    let info = await transporter.sendMail({
-      from: `anandsaiii1200@gmail.com`,
+    const mailOptions = {
+      from: 'anandsaiii1200@gmail.com',
       to: user.email,
-      subject: "AUTH - Reset Password",
-      text: `You are receiving this because you have requested the reset of the password of your account.\n\nToken: ${token}\n\nIf you didn't request this, please ignore this email and your password will remain unchanged.`,
-      html: `<p>You are receiving this because you have requested the reset of the password of your account.</p><p><strong>Token: ${token}</strong></p><p>If you didn't request this, please ignore this email and your password will remain unchanged.</p>`,
-    });
+      subject: 'Password Reset',
+      text:
+        'You are receiving this email because you requested a password reset. Please click on the following link to reset your password:',
+      html: `<a href="http://localhost:5000/api/users/reset-password/:token/${token}">Reset Password</a>`,
+    };
 
-    return res.json({
-      message: `An email has been sent to ${user.email} with further instructions`,
+    await transporter.sendMail(mailOptions);
+
+    res.json({
+      message: 'An email has been sent with further instructions.',
     });
   } catch (error) {
-    return res.status(400).json({ error: error.message });
+    console.log(error);
+    res.status(500).json({ error: 'Internal server error' });
   }
 };
 
